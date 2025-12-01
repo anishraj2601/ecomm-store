2e5e9ba511f64147516d1e56b457ee2cf3f82e5d
Anish <anishraj2601@gmail.com>
2025-12-01 22:41:55 +0530

    Add Cart and Shipping pages

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -1,15 +1,26 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'Ecomm Store';
-export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_APP_DESCRIPTION || 'Modern ecomm store';
-export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'https://localhost:3000';
-export const LATEST_PRODUCTS_LIMIT = Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || "Ecomm Store";
+export const APP_DESCRIPTION =
+  process.env.NEXT_PUBLIC_APP_DESCRIPTION || "Modern ecomm store";
+export const SERVER_URL =
+  process.env.NEXT_PUBLIC_SERVER_URL || "https://localhost:3000";
+export const LATEST_PRODUCTS_LIMIT =
+  Number(process.env.LATEST_PRODUCTS_LIMIT) || 4;
 export const signInDefaultValues = {
-    email: '',
-    password: '',
-}
+  email: "",
+  password: "",
+};
 
 export const signUpDefaultValues = {
-    name: '',
-    email: '',
-    password: '',
-    confirmPassword: '',
-}
+  name: "",
+  email: "",
+  password: "",
+  confirmPassword: "",
+};
+
+export const shippingAddressDefaultValues = {
+  fullName: "",
+  streetAddress: "",
+  city: "",
+  postalCode: "",
+  country: "",
+};
